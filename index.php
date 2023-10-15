995dd7bcd1145f4ae7883ab3d2c420afc9efda7c
r1d3rzz <myintthwaykhine127@gmail.com>
2023-10-15 21:45:03 +0630

    created caculator

--- index.php
+++ index.php
@@ -1,3 +1,58 @@
+<?php
+
+function dd($data = "")
+{
+    echo "<pre>";
+    die(var_dump($data));
+}
+
+$result = "";
+
+if (isset($_POST['calculate'])) {
+    $fnum = intval($_POST["fnum"]);
+    $snum = intval($_POST["snum"]);
+    $mathSign = $_POST["mathSign"];
+    $has_error = false;
+
+    if (is_string($fnum) || $fnum == "") {
+        $has_error = true;
+    }
+
+    if (is_string($snum) || $snum == "") {
+        $has_error = true;
+    }
+
+
+    if ($has_error === false) {
+        switch ($mathSign) {
+            case '+':
+                $result = $fnum + $snum;
+                break;
+            case '-':
+                $result = $fnum - $snum;
+                break;
+            case '*':
+                $result = $fnum * $snum;
+                break;
+            case '/':
+                if ($fnum !== 0 || $snum !== 0) {
+                    $result = $fnum / $snum;
+                } else {
+                    $result = "Divided Value not to be Zero";
+                }
+                break;
+            default:
+                $result = "ERROR: Your Input Must Be Number";
+                break;
+        }
+    } else {
+        $result = "ERROR: Your Input Must Be Number";
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,66 +60,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <title>Form</title>
+    <title>Caculator</title>
 </head>
 
 <body class="bg-dark">
     <div class="container">
         <div class="row">
-            <div class="col-5 mx-auto mt-5">
-                <form action="./signUp.php" method="POST">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="form-group mb-3">
-                                <label for="name">Username</label>
-                                <input type="text" name="name" id="name" placeholder="Username" class="form-control">
-                            </div>
-
-                            <div class="form-group mb-3">
-                                <label for="email">Email</label>
-                                <input type="email" name="email" id="email" placeholder="Email" class="form-control">
-                            </div>
-
-                            <div class="form-group mb-3">
-                                <label>Gender</label>
-                                <div class="d-flex">
-                                    <div class="me-3">
-                                        <input type="radio" name="gender" id="male" value="male">
-                                        <label class="ms-1" for="male">Male</label>
-                                    </div>
-
-                                    <div class="me-3">
-                                        <input type="radio" name="gender" id="gender" value="female">
-                                        <span class="ms-1" id="">Female</span>
-                                    </div>
-
-                                    <div class="me-3">
-                                        <input type="radio" name="gender" id="gender" value="Other">
-                                        <span class="ms-1">other</span>
-                                    </div>
+            <div class="col-7 mx-auto mt-5">
+                <div class="card">
+                    <div class="row">
+                        <div class="col">
+                            <form action="" method="POST">
+                                <div class="card-body d-flex">
+                                    <input type="text" name="fnum" placeholder="Enter First Number" class="form-control" required>
+                                    <select name="mathSign" class="form-select text-center">
+                                        <option value="+">&plus;</option>
+                                        <option value="-">&minus;</option>
+                                        <option value="*">&times;</option>
+                                        <option value="/">&divide;</option>
+                                    </select>
+                                    <input type="text" name="snum" placeholder="Enter Second Number" class="form-control" required>
+                                    <input type="submit" name="calculate" value="Calculate" class="btn btn-sm btn-dark ms-2">
                                 </div>
-                            </div>
-
-                            <div class="form-group mb-3">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" id="password" class="form-control">
-                            </div>
-
-                            <div class="form-group mb-3">
-                                <select name="townships" id="township" class="form-select">
-                                    <option value="" disabled selected>Choose Your Township</option>
-                                    <option value="yankin">Yankin</option>
-                                    <option value="ookala">Ookala</option>
-                                    <option value="thingangyun">Thingangyun</option>
-                                </select>
-                            </div>
-
-                            <div class="form-group text-end">
-                                <button class="btn btn-sm btn-primary">Sign Up</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="card card-body border-0">
+                                <label for="result">Result</label>
+                                <input type="text" name="result" id="result" class="form-control" value="<?= $result; ?>" style="height: 100px;">
                             </div>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
